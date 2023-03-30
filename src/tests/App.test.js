5d31931e02366f3465a0b2fda7b54cb60517ef77
gustavomorais <gustavomorais.dev@gmail.com>
2023-03-30 14:05:28 -0300

    completa o requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {}); // Init
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../App';
+
+describe('Teste do componente App', () => {
+  test('renders App component', () => {
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+    const linkHome = screen.getByText(/home/i);
+    const linkAbout = screen.getByText(/about/i);
+    const linkFavoritePokemon = screen.getByText(/favorite pokémon/i);
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavoritePokemon).toBeInTheDocument();
+  });
+});

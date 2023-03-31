93e83910154b05ffd2a213dd598c8209f7fb56f3
gustavomorais <gustavomorais.dev@gmail.com>
2023-03-31 07:49:51 -0300

    termina os testes para o NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Testes do NotFound', () => {
+  it('A página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent('Page requested not found');
+  });
+
+  it('A página mostra a imagem correta', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
